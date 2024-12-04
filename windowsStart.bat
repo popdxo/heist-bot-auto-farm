9a1202383cc319c0cae7d960da718fdf9cb322d8
brick <63986294+popdxo@users.noreply.github.com>
2024-12-03 23:30:36 -0500

    fixed windows 10 auto setup

--- windowsStart.bat
+++ windowsStart.bat
@@ -1,1 +1,38 @@
-sh start.sh
+@echo off
+
+call ignore.bat
+
+if "%completed%" == "false" (
+    echo paste in your discord token
+    set /p token=
+    echo paste in the discord channel id you would like to use (must be a server with the bot invited)
+    set /p channel=
+    echo setting up..
+
+    echo token = "%token%";> config.js
+    echo channelId = "%channel%";>> config.js
+    echo.>> config.js
+    echo module.exports = {>> config.js
+    echo.  token: "%token%",>> config.js
+    echo.  channelId: "%channel%" >> config.js
+    echo };>> config.js
+
+    echo set completed=true> ignore.bat
+)
+
+echo [1^| Run the bot] [2^| Run setup again] [3^| Exit]
+set /p number=
+if "%number%" == "1" (
+    echo Running
+    node run.js
+) else if "%number%" == "2" (
+    echo Resetting setup..
+    echo set completed=false> ignore.bat
+    call windowsStart.bat
+) else if "%number%" == "3" (
+    echo Exiting..
+    exit /b 0
+) else (
+    echo Invalid number. Exiting..
+    exit /b 0
+)
