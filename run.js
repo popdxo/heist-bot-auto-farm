30bd44dc0f3cc8feafa04787851dc4a7186a945a
brick <63986294+popdxo@users.noreply.github.com>
2024-12-03 23:13:45 -0500

    added troubloshooters

--- run.js
+++ run.js
@@ -6,8 +6,18 @@ function autoBegStart() {
     console.log(`AutoBeg: ${data}`);
   });
   autoBeg.on("error", (error) => {
-    console.log(`AutoBeg error: ${error.message}`);
-    setTimeout(autoBegStart(), 10 * 1000);
+    console.log(`AutoBeg error: ${error.message} (report this error: 1)`);
+  });
+  autoBeg.stderr.on("data", (data) => {
+    console.error(
+      `AutoBeg stderr: ${data} (error: 2 please help me fix this error)`,
+    );
+    setTimeout(autoBegStart, 10 * 1000);
+  });
+  autoBeg.on("close", (code) => {
+    console.log(
+      `AutoBeg process exited with code: ${code} (report this error: 3)`,
+    );
   });
 }
 
@@ -17,8 +27,19 @@ function autoWorkStart() {
     console.log(`AutoWork: ${data}`);
   });
   autoWork.on("error", (error) => {
-    console.log(`AutoWork error: ${error.message}`);
-    setTimeout(autoWorkStart(), 10 * 1000);
+    console.log(`AutoWork error: ${error.message} (report this error: 1)`);
+    console.log("Restarting autoWork in 10 seconds");
+    setTimeout(autoWorkStart, 10 * 1000);
+  });
+  autoWork.stderr.on("data", (data) => {
+    console.error(
+      `AutoWork stderr: ${data} (error: 2 please help me fix this error)`,
+    );
+  });
+  autoWork.on("close", (code) => {
+    console.log(
+      `AutoWork process exited with code: ${code} (report this error: 3)`,
+    );
   });
 }
 
